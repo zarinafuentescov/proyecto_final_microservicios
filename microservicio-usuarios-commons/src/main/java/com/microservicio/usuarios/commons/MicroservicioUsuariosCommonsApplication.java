05faa0d14dbfcce100b6802fffed759b1946a786
Zari <zarinafuentescov@gmail.com>
2021-06-22 18:50:46 -0400

    microservicio commons usuarios

---
+++ microservicio-usuarios-commons/src/main/java/com/microservicio/usuarios/commons/MicroservicioUsuariosCommonsApplication.java
@@ -0,0 +1,14 @@
+package com.microservicio.usuarios.commons;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class MicroservicioUsuariosCommonsApplication {
+
+	
+
+}
---
+++ microservicio-usuarios-commons/src/test/java/com/microservicio/usuarios/commons/MicroservicioUsuariosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.usuarios.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsuariosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
